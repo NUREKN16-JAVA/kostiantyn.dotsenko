75a48ec380d79f1a8b5e50a435ebdbcdc64ffdd8
kd935 <32416062+kd935@users.noreply.github.com>
2018-11-13 22:01:23 +0200

    Initial commit for usermanagement

---
+++ usermanagement/src/main/java/ua/nure/kn/dotsenko/usermanagement/User.java
@@ -0,0 +1,128 @@
+package ua.nure.kn.dotsenko.usermanagement;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
+
+public class User implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3278038741672885759L;
+	
+	@Override
+	public String toString() {
+		return "User [firstNAme=" + firstName + ", lastName=" + lastName + ", dateOfBirth=" + dateOfBirth + ", id=" + id
+				+ "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dateOfBirth == null) ? 0 : dateOfBirth.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (dateOfBirth == null) {
+			if (other.dateOfBirth != null)
+				return false;
+		} else if (!dateOfBirth.equals(other.dateOfBirth))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		return true;
+	}
+	private String firstName;
+	private String lastName;
+	private Date dateOfBirth;
+	private Long id;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstNAme) {
+		this.firstName = firstNAme;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFullName() {
+		
+		return getFirstName() + ", " + getLastName();
+		
+	}
+	public long getAge() {
+		
+		Calendar currentTime = Calendar.getInstance();
+		Calendar _dateOfBirth = Calendar.getInstance();
+		
+		_dateOfBirth.setTime(getDateOfBirth());
+		
+		
+		if (_dateOfBirth.after(currentTime)) {
+		  throw new IllegalArgumentException("Wrong Date!");
+		}
+		
+		long currentYear = currentTime.get(Calendar.YEAR);
+		long yearOfBirth = _dateOfBirth.get(Calendar.YEAR);
+		long age = currentYear - yearOfBirth;
+		
+		long currentMonth = currentTime.get(Calendar.MONTH);
+		//currentMonth++;
+		long monthOfBirth = _dateOfBirth.get(Calendar.MONTH);
+		
+		if (monthOfBirth > currentMonth) {
+		  age--;
+		} else if (currentMonth == monthOfBirth) {
+			long currentDay = currentTime.get(Calendar.DAY_OF_MONTH);
+			long dayOfBirth = _dateOfBirth.get(Calendar.DAY_OF_MONTH);
+		  
+		    if (dayOfBirth > currentDay) {
+		    age--;
+		    }
+		}
+	
+		return age;
+	
+}
+}
---
+++ usermanagement/src/test/java/ua/nure/kn/dotsenko/usermanagement/TestUser.java
@@ -0,0 +1,123 @@
+package ua.nure.kn.dotsenko.usermanagement;
+
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class TestUser {
+
+	private static final String FULL_NAME_ETALON = "Ivan, Ivanov";
+	private static final String LAST_NAME = "Ivanov";
+	private static final String FIRST_NAME = "Ivan";
+	
+	// Тест актуальный на дату 13.11.2018
+	private static final int CURRENT_YEAR = 2018;
+	
+	private static final int YEAR_OF_BIRTH = 1999;
+	private User user;
+	
+	// Тест Возраста 1 - для случая, где день рождения уже прошёл, но месяца совпадают
+
+	private static final int ETALONE_AGE_1 = CURRENT_YEAR - YEAR_OF_BIRTH;
+	private static final int DAY_OF_BIRTH_1 = 10;
+	private static final int MONTH_OF_BIRTH_1 = 10;
+	
+	@Test
+	public void testGetAge1() {
+		
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(YEAR_OF_BIRTH, MONTH_OF_BIRTH_1, DAY_OF_BIRTH_1);
+		user.setDateOfBirth(calendar.getTime());
+		
+		assertEquals(ETALONE_AGE_1, user.getAge());
+	}
+	
+	// Тест Возраста 2 - для случая, когда день рождения совпадает с текущим днём
+
+		private static final int ETALONE_AGE_2 = CURRENT_YEAR - YEAR_OF_BIRTH;
+		private static final int DAY_OF_BIRTH_2 = 13;
+		private static final int MONTH_OF_BIRTH_2 = 10;
+		
+		@Test
+		public void testGetAge2() {
+			
+			Calendar calendar = Calendar.getInstance();
+			calendar.set(YEAR_OF_BIRTH, MONTH_OF_BIRTH_2, DAY_OF_BIRTH_2);
+			user.setDateOfBirth(calendar.getTime());
+			
+			assertEquals(ETALONE_AGE_2, user.getAge());
+		}
+	
+	// Тест Возраста 3 - для случая, когда день рождения завтра
+
+		private static final int ETALONE_AGE_3 = CURRENT_YEAR - YEAR_OF_BIRTH - 1;
+		private static final int DAY_OF_BIRTH_3 = 14;
+		private static final int MONTH_OF_BIRTH_3 = 10;
+				
+		@Test
+		public void testGetAge3() {
+					
+			Calendar calendar = Calendar.getInstance();
+			calendar.set(YEAR_OF_BIRTH, MONTH_OF_BIRTH_3, DAY_OF_BIRTH_3);
+			user.setDateOfBirth(calendar.getTime());
+					
+			assertEquals(ETALONE_AGE_3, user.getAge());
+		}
+		
+	// Тест Возраста 4 - для случая, когда день рождения 1-го января
+
+		private static final int ETALONE_AGE_4 = CURRENT_YEAR - YEAR_OF_BIRTH;
+		private static final int DAY_OF_BIRTH_4 = 1;
+		private static final int MONTH_OF_BIRTH_4 = 0;
+						
+		@Test
+		public void testGetAge4() {
+							
+			Calendar calendar = Calendar.getInstance();
+			calendar.set(YEAR_OF_BIRTH, MONTH_OF_BIRTH_4, DAY_OF_BIRTH_4);
+			user.setDateOfBirth(calendar.getTime());
+							
+			assertEquals(ETALONE_AGE_4, user.getAge());
+		}
+		
+		// Тест Возраста 5 - для случая, когда день рождения 31-го декабря
+
+				private static final int ETALONE_AGE_5 = CURRENT_YEAR - YEAR_OF_BIRTH - 1;
+				private static final int DAY_OF_BIRTH_5 = 31;
+				private static final int MONTH_OF_BIRTH_5 = 11;
+								
+				@Test
+				public void testGetAge5() {
+									
+					Calendar calendar = Calendar.getInstance();
+					calendar.set(YEAR_OF_BIRTH, MONTH_OF_BIRTH_5, DAY_OF_BIRTH_5);
+					user.setDateOfBirth(calendar.getTime());
+									
+					assertEquals(ETALONE_AGE_5, user.getAge());
+				}
+		
+	
+		
+	@Before
+	public void setUp() throws Exception {
+		user = new User();
+
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetFullName() {
+	user.setFirstName(FIRST_NAME);
+  	user.setLastName(LAST_NAME);
+     assertEquals(FULL_NAME_ETALON, (String)user.getFullName());
+	}
+
+}
